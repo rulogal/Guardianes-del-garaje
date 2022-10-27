72785b9360e57cd1bcbece4be401e100de2d3294
Rulogal <raulgasa97@gmail.com>
2022-10-27 10:51:18 -0500

    Guardianes del garaje - Next Js init 🚀👩‍🚀.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/index.css";
+import React, { useEffect } from "react";
+import { useRouter, Router } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const pageView = (url, title) => {
+    window &&
+      window.dataLayer &&
+      window.dataLayer.push({
+        event: "virtualPageview",
+        virtualPageURL: url,
+        virtualPageTitle: title,
+      });
+  };
+  useEffect(() => {
+    pageView(router.pathname, document.title);
+    const handleRouteChange = (url) => {
+      pageView(url, document.title);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
